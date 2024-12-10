867732178850aab77d8df970b4b2b5e1c753d57f
Niklas Mähler <2573608+maehler@users.noreply.github.com>
2024-12-10 08:36:58 +0100

    Day 5, part 2

--- lib/day5.ml
+++ lib/day5.ml
@@ -31,14 +31,37 @@ let is_valid_update rules update =
     List.for_all (pairs [] update) ~f:(fun (first, second) ->
         not (List.mem rules (second, first) ~equal:(fun (a, b) (a2, b2) -> a = a2 && b = b2)))
 
+let fix_update rules unordered_update =
+    let rec aux acc update =
+        if (Set.length update) = 0 then List.rev acc
+        else
+            let next = Set.find_exn update ~f:(fun n1 ->
+                not(Set.exists update ~f:(fun n2 ->
+                    List.mem rules (n2, n1) ~equal:(fun (a, b) (a2, b2) -> a = a2 && b = b2)
+                ))) in
+            aux (next :: acc) (Set.remove update next)
+    in
+    let ordered_update = aux [] (Set.of_list (module Int) unordered_update) in
+    assert (is_valid_update rules ordered_update);
+    ordered_update
+
 let part1 filename =
     let (rules, updates) = read filename |> parse in
     let valid_updates = List.map updates ~f:(fun u -> is_valid_update rules u) in
     List.filteri updates ~f:(fun i _ -> List.nth_exn valid_updates i) |>
     List.map ~f:(fun update ->
-        let len = List.length update in
-        let middle = List.nth_exn update (len / 2) in
-        middle
+        List.nth_exn update ((List.length update) / 2)
+    ) |>
+    List.fold_left ~f:(+) ~init:0 |>
+    Printf.printf "%d\n"
+
+let part2 filename = 
+    let (rules, updates) = read filename |> parse in
+    let invalid_updates = List.map updates ~f:(fun u -> not (is_valid_update rules u)) in
+    List.filteri updates ~f:(fun i _ -> List.nth_exn invalid_updates i) |>
+    List.map ~f:(fun u -> fix_update rules u) |>
+    List.map ~f:(fun update ->
+        List.nth_exn update ((List.length update) / 2)
     ) |>
     List.fold_left ~f:(+) ~init:0 |>
     Printf.printf "%d\n"
@@ -46,4 +69,5 @@ let part1 filename =
 let run filename part =
     match part with
     | 1 -> part1 filename
+    | 2 -> part2 filename
     | _ -> failwith "invalid part"
